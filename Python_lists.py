d243974b36df4adde209b2e6be5e509c62bb4b8c
DavidEBlurton <163071893+DavidEBlurton@users.noreply.github.com>
2024-07-18 14:21:34 -0700

    first commit

---
+++ Python_lists.py
@@ -0,0 +1,12 @@
+# TASK 1
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+# grades.sort(reverse=True)
+# print(grades)
+
+# Task 2
+
+total=sum(grades)
+print(total)
+print(len(grades))
+print(sum(grades)/len(grades))
